d43d21b8e474aa4bcdfd463663ea414fb1bd5541
coco <coco.shih@croxera.com>
2025-03-06 16:23:39 +0800

    Initial Commit

---
+++ HiSOLID/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HiSOLID
+//
+//  Created by CocoShih on 2025/3/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HiSOLIDTests/HiSOLIDTests.swift
@@ -0,0 +1,17 @@
+//
+//  HiSOLIDTests.swift
+//  HiSOLIDTests
+//
+//  Created by CocoShih on 2025/3/6.
+//
+
+import Testing
+@testable import HiSOLID
+
+struct HiSOLIDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
